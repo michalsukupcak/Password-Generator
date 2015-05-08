b8a1bc74c7bcf092a1d206c167e090d972f8d7e1
michalsukupcak <bloodspellsvk@gmail.com>
2015-05-08 17:57:56 +0200

    Brand new version of application. AngularJS has been removed and app was rewritten to use only Polymer.

--- app/App.js
+++
@@ -1,6 +0,0 @@
-/**
- * Initializes main app variable and injects required modules.
- * 
- * @author Michal Sukupčák <sukupcak@webdesign-studio.sk>
- */
-var app = angular.module('wds-password-generator',['ngRoute','ng-polymer-elements','ngStorage']);
